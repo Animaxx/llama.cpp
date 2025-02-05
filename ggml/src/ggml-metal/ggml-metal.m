1191c822576677cc4cb152887316918cb3016617
Animax Deng <Animax.deng@gmail.com>
2025-02-05 10:10:23 -0600

    https://github.com/ggerganov/llama.cpp/commit/d774ab3acc4fee41fbed6dbfc192b57d5f79f34b#diff-4045ccc2edf36af53c4a28fca4a6a24f628b3c9bee7ccb43b06e55dc5e5e199e

--- ggml/src/ggml-metal/ggml-metal.m
+++ ggml/src/ggml-metal/ggml-metal.m
@@ -1194,7 +1194,7 @@ static bool ggml_metal_supports_op(const struct ggml_backend_metal_device_contex
         case GGML_OP_SUM_ROWS:
         case GGML_OP_SOFT_MAX:
         case GGML_OP_GROUP_NORM:
-            return has_simdgroup_reduction;
+            return has_simdgroup_reduction && (op->ne[0] % 4 == 0 && ggml_is_contiguous_1(op->src[0]));
         case GGML_OP_RMS_NORM:
             return has_simdgroup_reduction && (op->ne[0] % 4 == 0);
         case GGML_OP_ARGMAX:
